5c97034a1914f7801e51edc2aaef9a57a9ab9dac
Begoenix <43401736+Begoenix@users.noreply.github.com>
2019-09-14 17:40:13 +0800

    Delete TextRanker示例.py

--- TextRank/TextRanker示例.py
+++
@@ -1,9 +0,0 @@
-import TextRanker as TR
-In = ["我/w","爱/v","中国/n","中国/n","爱/v","我/w"]
-SI = TR.RandomScoreInitializer(In)
-Score = SI.Unit()
-Graph = TR.CooccurenceConstructor(dict_weight = Score,list_relation = SI.object,span = 1)
-Ranker = TR.TextRanker(d = 0.8,graph = Graph.graph)
-Ranker.Rank(args = 10)
-out = Ranker.get_token_score("我")
-print(out)
